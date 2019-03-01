bff9af60f2c882b954c790a8dc7f93cb8fd35761
papppeter <papppeter@gmail.com>
2019-03-01 15:12:52 +0100

    basic func for yii2 to work with laravel components tested with https://github.com/darryldecode/laravelshoppingcart

---
+++ Illuminate/Events/Dispatcher.php
@@ -0,0 +1,160 @@
+<?php
+namespace papppeter\yii2Laravel\Illuminate\Events;
+
+use yii\base\Component;
+use yii\base\Event;
+use yii\base\UnknownMethodException;
+
+/**
+ *
+ */
+class Dispatcher extends Event {
+    /**
+     * Register an event listener with the dispatcher.
+     *
+     * @param  string|array $events
+     * @param  mixed $listener
+     * @return void
+     */
+    public function listen($events, $listener)
+    {
+        foreach ((array) $events as $event) {
+            app()->on($event, $listener);
+        }
+    }
+
+    /**
+     * Determine if a given event has listeners.
+     *
+     * @param  string $eventName
+     * @return bool
+     */
+    public function hasListeners($eventName)
+    {
+        return app()->hasEventHandlers($eventName);
+    }
+
+    /**
+     * Register an event subscriber with the dispatcher.
+     *
+     * @param  object|string $subscriber
+     * @return void
+     */
+    public function subscribe($subscriber)
+    {
+        throw new UnknownMethodException();
+    }
+
+    /**
+     * Dispatch an event until the first non-null response is returned.
+     *
+     * @param  string|object $event
+     * @param  mixed $payload
+     * @return array|null
+     */
+    public function until($event, $payload = [])
+    {
+        throw new UnknownMethodException();
+    }
+
+    /**
+     * Dispatch an event and call the listeners.
+     *
+     * @param  string|object $event
+     * @param  mixed $payload
+     * @param  bool $halt
+     * @return array|null
+     */
+    public function dispatch($event, $payload = [], $halt = false)
+    {
+        if($event instanceof Event) {
+            return app()->trigger($event->name, $event);
+        }
+
+        list($event, $payload) = $this->parseEventAndPayload(
+            $event, $payload
+        );
+
+        $name = $event;
+        $event = new Event();
+        $event->data = $payload[0];
+        $event->sender = $payload[1];
+        $event->handled = $halt;
+
+        return app()->trigger($name, $event);
+    }
+
+    /**
+     * Register an event and payload to be fired later.
+     *
+     * @param  string $event
+     * @param  array $payload
+     * @return void
+     */
+    public function push($event, $payload = [])
+    {
+        throw new UnknownMethodException();
+    }
+
+    /**
+     * Flush a set of pushed events.
+     *
+     * @param  string $event
+     * @return void
+     */
+    public function flush($event)
+    {
+        throw new UnknownMethodException();
+    }
+
+    /**
+     * Remove a set of listeners from the dispatcher.
+     *
+     * @param  string $event
+     * @return void
+     */
+    public function forget($event)
+    {
+        app()->off($event);
+    }
+
+    /**
+     * Forget all of the queued listeners.
+     *
+     * @return void
+     */
+    public function forgetPushed()
+    {
+        throw new UnknownMethodException();
+    }
+
+    /**
+     * Fire an event and call the listeners.
+     *
+     * @param  string|object  $event
+     * @param  mixed  $payload
+     * @param  bool  $halt
+     * @return array|null
+     */
+    public function fire($event, $payload = [], $halt = false)
+    {
+        return $this->dispatch($event, $payload, $halt);
+    }
+
+
+    /**
+     * Parse the given event and payload and prepare them for dispatching.
+     *
+     * @param  mixed  $event
+     * @param  mixed  $payload
+     * @return array
+     */
+    protected function parseEventAndPayload($event, $payload)
+    {
+        if (is_object($event)) {
+            [$payload, $event] = [[$event], get_class($event)];
+        }
+
+        return [$event, array_wrap($payload)];
+    }
+}
---
+++ Illuminate/Session/Store.php
@@ -0,0 +1,184 @@
+<?php
+namespace papppeter\yii2Laravel\Illuminate\Session;
+
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Support\Arr;
+
+/**
+ *
+ * @property string $previousUrl
+ * @property \Illuminate\Http\Request $requestOnHandler
+ */
+class Store extends \yii\web\Session implements Session {
+
+    public function put($key, $value = null) {
+        $this->set($key, $value);
+    }
+
+    /**
+     * Start the session, reading the data from a handler.
+     *
+     * @return bool
+     */
+    public function start()
+    {
+        return $this->open();
+    }
+
+    /**
+     * Save the session data to storage.
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        $this->freeze();
+    }
+
+    /**
+     * Get all of the session data.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $_SESSION ?? [];
+    }
+
+    /**
+     * Checks if a key exists.
+     *
+     * @param  string|array $key
+     * @return bool
+     */
+    public function exists($key)
+    {
+        return ! collect(is_array($key) ? $key : func_get_args())->contains(function ($key) {
+            return ! Arr::exists(array_keys($this->all()), $key);
+        });
+    }
+
+    /**
+     * Checks if an a key is present and not null.
+     *
+     * @param  string|array $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return ! collect(is_array($key) ? $key : func_get_args())->contains(function ($key) {
+            return is_null($this->get($key));
+        });
+    }
+
+    /**
+     * Get the CSRF token value.
+     *
+     * @return string
+     */
+    public function token()
+    {
+        return $this->get('_token');
+    }
+
+    /**
+     * Remove one or many items from the session.
+     *
+     * @param  string|array $keys
+     * @return void
+     */
+    public function forget($keys)
+    {
+        foreach(array_wrap($keys) as $key) {
+            $this->remove($key);
+        }
+    }
+
+    /**
+     * Remove all of the items from the session.
+     *
+     * @return void
+     */
+    public function flush()
+    {
+        $this->removeAll();
+    }
+
+    /**
+     * Generate a new session ID for the session.
+     *
+     * @param  bool $destroy
+     * @return bool
+     */
+    public function migrate($destroy = false)
+    {
+        $this->regenerateID($destroy);
+
+        return true;
+    }
+
+    /**
+     * Determine if the session has been started.
+     *
+     * @return bool
+     */
+    public function isStarted()
+    {
+        return $this->getIsActive();
+    }
+
+    /**
+     * Get the previous URL from the session.
+     *
+     * @return string|null
+     */
+    public function previousUrl()
+    {
+        return $this->get('_previous.url');
+    }
+
+    /**
+     * Set the "previous" URL in the session.
+     *
+     * @param  string $url
+     * @return void
+     */
+    public function setPreviousUrl($url)
+    {
+        $this->put('_previous.url', $url);
+    }
+
+    /**
+     * Get the session handler instance.
+     *
+     * @return \SessionHandlerInterface
+     */
+    public function getHandler()
+    {
+        return $this->handler;
+    }
+
+    /**
+     * Determine if the session handler needs a request.
+     *
+     * @return bool
+     */
+    public function handlerNeedsRequest()
+    {
+        return false;
+//        return $this->handler instanceof CookieSessionHandler;
+    }
+
+    /**
+     * Set the request on the handler instance.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return void
+     */
+    public function setRequestOnHandler($request)
+    {
+        if ($this->handlerNeedsRequest()) {
+            $this->handler->setRequest($request);
+        }
+    }
+}
